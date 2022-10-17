7208b222d818402c2c39c104267d797b5818c46b
simon* <simonpatry12@gmail.com>
2022-10-17 17:02:23 +0200

    feat: navbar responsive | home on-going

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,10 +1,16 @@
 import React, {useEffect, useState} from "react";
+import { useMediaQuery } from 'react-responsive';
+import "./home.css";
 
 const Home = () => {
+
+    const isMobile = useMediaQuery({ query: '(max-width: 480px)' })
+    const isTablet = useMediaQuery({ query: '(max-width: 768px)' })
+    let screenSize = isMobile ? "mobile" : isTablet ? "tablet" : "desktop";
     return(
-        <>
+        <div className={screenSize}>
             <h1>Home</h1>
-        </>
+        </div>
     )
 }
 
--- src/Components/navBar/SideNav.jsx
+++ src/Components/navBar/SideNav.jsx
@@ -4,20 +4,32 @@ import {
   Routes,
   Route, Link,
 } from "react-router-dom";
-import "./sideNav.css"
+import "./sideNav.css";
 import Home from "../Home/Home";
 import Projects from "../Projects/Projects";
 import Contact from "../Contact/Contact";
 import About from "../About/About";
 import {Button} from "@mui/material";
+import { useMediaQuery } from 'react-responsive';
 
 const SideNav = () => {
     const [state, setState] = useState({showNav : false});
     let navCoverStyle = { width: state.showNav ? "100%" : "0" }
     let sideNavStyle = { width: state.showNav ? "250px" : "0" }   
+    
+    // Handle navbar for responsive
+    const isMobile = useMediaQuery({ query: '(max-width: 480px)' })
+    const isTablet = useMediaQuery({ query: '(max-width: 768px)' })
+    let screenSize = isMobile ? "mobile" : isTablet ? "tablet" : "desktop";
+    useEffect(() => {
+        if(screenSize === "desktop")
+            setState({...state, showNav: true});
+    },[screenSize])
 
     // open/close navabar
-    const handleNavClick = () => {
+    const handleNavClick = (e = null) => {
+        if (e)
+            e.preventDefault();
         setState(state.showNav ? {...state, showNav: false} : {...state, showNav: true});
     }
 
@@ -25,32 +37,42 @@ const SideNav = () => {
         <>
             <Router>
 
-                {/* OPening navbar button */}
-                <Button onClick={(e) => {
-                        e.preventDefault();
-                        handleNavClick();
-                    }}
-                    className="open-nav"
-                >
-                    &#9776;
-                </Button>
+                {screenSize !== "desktop" && 
+                    <>
+                    {/* Opening navbar button */}
+                        <Button onClick={(e) => {
+                                e.preventDefault();
+                                handleNavClick();
+                            }}
+                            className="open-nav"
+                        >
+                            &#9776;
+                        </Button>
+                    
+                    {/* Covering th e screen with a div beneath the navbar to control opening */}
+                        <div
+                            onClick={handleNavClick}
+                            className="nav-cover"
+                            style={navCoverStyle}
+                        />
+                    </>
+                }
 
-                {/* Covering th e screen with a div beneath the navbar to control opening */}
-                <div
-                    onClick={handleNavClick}
-                    className="nav-cover"
-                    style={navCoverStyle}
-                />
+                
 
                 {/* Navigation */}
                 <div name="side-nav" className="side-nav" style={sideNavStyle}>
-                    <a href="" onClick={handleNavClick} className="close-nav">
-                    &times;
-                    </a>
-                    <Link onClick={handleNavClick} to="/">Accueil</Link>
-                    <Link onClick={handleNavClick} to="/projects">Projets</Link>
-                    <Link onClick={handleNavClick} to="/aboutMe">A propos</Link>
-                    <Link onClick={handleNavClick} to="/contact">Contactez moi</Link>
+                    {screenSize !== "desktop" && 
+                        <a href="javascript:;" onClick={handleNavClick} 
+                            className="close-nav"
+                        >
+                            &times;
+                        </a>
+                    }
+                    <Link to="/">Accueil</Link>
+                    <Link to="/projects">Projets</Link>
+                    <Link to="/aboutMe">A propos</Link>
+                    <Link to="/contact">Contactez moi</Link>
                 </div>
                 <div>
                     <Routes>
